ea9b5863b82cef1cb633ca97ca18d54d76a2fa7a
Bryann Alfaro <alf19372@uvg.edu.gt>
2021-11-04 19:54:59 -0600

    feat: adding some details

--- life_generator.py
+++ life_generator.py
@@ -1,8 +1,9 @@
 import pygame
 import random
 
-BLACK  = (  0,   25,   255)
-WHITE = (255,255,255)
+BACKGROUND  = (  0,   25,   255)
+CPOINT = (255,255,255)
+
 class Life(object):
     def __init__(self, screen):
         self.screen = screen
@@ -11,58 +12,53 @@ class Life(object):
         _,_,self.width,self.height = self.screen.get_rect()
 
     def clear(self):
-        self.screen.fill(BLACK)
+        self.screen.fill(BACKGROUND)
 
-    def pixel(self, x, y,color = WHITE):
+    def pixel(self, x, y,color = CPOINT):
         self.screen.set_at((x, y), color)
 
     def copy(self):
         self.prev_turn = self.screen.copy()
 
     def render(self):
-        beacon = [[(15,10),(16,10),(15,9),(16,9),(17,8),(18,8),(17,7),(18,7)],[(15,10),(16,10),(15,9),(18,8),(17,7),(18,7)]]
-        tube = [(40,60),(41,59),(39,59),(40,58)]
-        cube = [[(10,10),(11,10),(12,10)],[(10,10),(10,11),(10,12)],[(10,12),(11,12),(12,12)],[(12,12),(12,11),(12,10)]]
         '''
-
+    Condiciones:
     Cualquier célula viva que tenga menos de dos vecinos vivos, muere. (underpopulation)
     Una célula viva que tenga dos o tres vecinos vivos, sobrevive. (survival)
     Cualquier célula viva que tenga más de tres vecinos vivos, muere. (overpopulation)
     Cualquier célula muerta que tenga exactamente tres vecinos vivos, vive. (reproduction)
 
         '''
-
         contador = 0
-        print(self.width, self.height)
         vecinos = 0
         for i in range (1,self.width-1):
             for j in range (1,self.height-1):
                 prev = self.prev_turn.get_at((i, j))
-                if prev == WHITE:
+                if prev == CPOINT:
                     try:
 
-                        if self.prev_turn.get_at((i+1, j)) == WHITE:
+                        if self.prev_turn.get_at((i+1, j)) == CPOINT:
                             vecinos += 1
-                        if self.prev_turn.get_at((i-1, j)) == WHITE:
+                        if self.prev_turn.get_at((i-1, j)) == CPOINT:
                             vecinos += 1
-                        if self.prev_turn.get_at((i, j+1)) == WHITE:
+                        if self.prev_turn.get_at((i, j+1)) == CPOINT:
                             vecinos += 1
-                        if self.prev_turn.get_at((i, j-1)) == WHITE:
+                        if self.prev_turn.get_at((i, j-1)) == CPOINT:
                             vecinos += 1
-                        if self.prev_turn.get_at((i+1, j+1)) == WHITE:
+                        if self.prev_turn.get_at((i+1, j+1)) == CPOINT:
                             vecinos += 1
-                        if self.prev_turn.get_at((i-1, j-1)) == WHITE:
+                        if self.prev_turn.get_at((i-1, j-1)) == CPOINT:
                             vecinos += 1
-                        if self.prev_turn.get_at((i+1, j-1)) == WHITE:
+                        if self.prev_turn.get_at((i+1, j-1)) == CPOINT:
                             vecinos += 1
-                        if self.prev_turn.get_at((i-1, j+1)) == WHITE:
+                        if self.prev_turn.get_at((i-1, j+1)) == CPOINT:
                             vecinos += 1
                         if vecinos < 2:
-                            self.pixel(i, j, BLACK)
+                            self.pixel(i, j, BACKGROUND)
                         elif vecinos == 2 or vecinos == 3:
-                            self.pixel(i, j, WHITE)
+                            self.pixel(i, j, CPOINT)
                         elif vecinos > 3:
-                            self.pixel(i, j, BLACK)
+                            self.pixel(i, j, BACKGROUND)
                         vecinos = 0
 
                     except:
@@ -72,36 +68,30 @@ class Life(object):
 
                 else:
                     try:
-                        if self.prev_turn.get_at((i+1, j)) == WHITE:
+                        if self.prev_turn.get_at((i+1, j)) == CPOINT:
                             vecinos += 1
-                        if self.prev_turn.get_at((i-1, j)) == WHITE:
+                        if self.prev_turn.get_at((i-1, j)) == CPOINT:
                             vecinos += 1
-                        if self.prev_turn.get_at((i, j+1)) == WHITE:
+                        if self.prev_turn.get_at((i, j+1)) == CPOINT:
                             vecinos += 1
-                        if self.prev_turn.get_at((i, j-1)) == WHITE:
+                        if self.prev_turn.get_at((i, j-1)) == CPOINT:
                             vecinos += 1
-                        if self.prev_turn.get_at((i+1, j+1)) == WHITE:
+                        if self.prev_turn.get_at((i+1, j+1)) == CPOINT:
                             vecinos += 1
-                        if self.prev_turn.get_at((i-1, j-1)) == WHITE:
+                        if self.prev_turn.get_at((i-1, j-1)) == CPOINT:
                             vecinos += 1
-                        if self.prev_turn.get_at((i+1, j-1)) == WHITE:
+                        if self.prev_turn.get_at((i+1, j-1)) == CPOINT:
                             vecinos += 1
-                        if self.prev_turn.get_at((i-1, j+1)) == WHITE:
+                        if self.prev_turn.get_at((i-1, j+1)) == CPOINT:
                             vecinos += 1
                         if vecinos ==3:
-                            self.pixel(i, j, WHITE)
+                            self.pixel(i, j, CPOINT)
                         vecinos=0
                     except:
                         vecinos=0
                         pass
                         contador += 1
                         print("Fuera",contador)
-
-
-
-
-
-        #print(self.prev_turn.get_at((x, y)))
 
 
 
--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 from life_generator import *
 import pygame
 import random
+from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((200, 200))
+width = 300
+height = 300
+screen = pygame.display.set_mode((width, height))
 r = Life(screen)
 
-[(15,10),
-(16,10),
-(15,9),
-(16,9),
-(17,8),
-(18,8),
-(17,7),
-(18,7)]
-
-for i in range(40):
-  first = random.randint(10,180)
-  second = random.randint(10,180)
+for i in range(int(width/10)*int(width/40)):
+  first = random.randint(10,width-10)
+  second = random.randint(10,height-10)
   r.pixel(second,first)
   r.pixel(second+1,first)
   r.pixel(second+2,first)
@@ -36,25 +30,29 @@ for i in range(40):
   r.pixel(first-1,second-2)
   r.pixel(first-2,second-1)
 
-'''r.pixel(44,40)
-r.pixel(45,40)
-r.pixel(46,40)
-
-r.pixel(40,41)
-r.pixel(41,41)
-r.pixel(42,41)'''
 
 myfont = pygame.font.SysFont('Comic Sans MS', 10)
+myfont2 = pygame.font.SysFont('Comic Sans MS', 9)
+
+
 
 generations = 0
 while True:
-  pygame.time.delay(300)
+  pygame.event.get()
+  pygame.time.delay(200)
   r.copy()
   r.clear()
   r.render()
   text = 'Generacion: ' + str(generations)
+  text2 = 'Hecho por: Bryann Alfaro'
   textsurface = myfont.render(text, False, (255, 255, 0))
-  screen.blit(textsurface,(0,0))
+  textsurface2 = myfont2.render(text2, False, (255, 255, 0))
+  screen.blit(textsurface,(width/2-80,height-20))
+  screen.blit(textsurface2,(width/2+10,height-20))
   generations += 1
+  for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
 
   pygame.display.flip()
